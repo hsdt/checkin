7c926437b386dd41fbfc35401e32ce051ecf2a3c
Vunb <nhubaovu@gmail.com>
2019-11-05 15:00:44 +0700

    update image profile

--- client/src/views/UserProfile.tsx
+++ client/src/views/UserProfile.tsx
@@ -129,7 +129,7 @@ class UserProfile extends Component {
             </Col>
             <Col md={4}>
               <UserCard
-                bgImage='https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400'
+                bgImage='https://avatars2.githubusercontent.com/u/3768250?v=4'
                 avatar={avatar}
                 name='Mike Andrew'
                 userName='michael24'
